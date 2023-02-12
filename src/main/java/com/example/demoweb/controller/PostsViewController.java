0cdcd7278ca72ab9ecbca2ac6fe332be64dd1be1
Мелёхина Виктория <sweet_fox7940107@mail.ru>
2023-02-12 21:50:25 +0500

    ALL

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -17,7 +17,7 @@ public class PostsViewController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
-        model.addAttribute("appName", "Сентябрь горит");
+        model.addAttribute("appName", "Моё супер приложение");
         model.addAttribute("posts", postsService.listAllPosts());
         return "list";
     }
